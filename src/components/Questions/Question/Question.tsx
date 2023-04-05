c8802b8b44f32a58e74a98aa654d96159edd2132
Dmitry <deus.main@gmail.com>
2023-04-05 15:29:37 +0300

    🚀feat(Question): add default checked value to radio inputs

--- src/components/Questions/Question/Question.tsx
+++ src/components/Questions/Question/Question.tsx
@@ -8,10 +8,10 @@ export const Question: FC<QuestionProps> = (props) => {
 		<article className={styles.question}>
 			<h3 className={styles.questionTitle}>{props.question}</h3>
 			<ul className={styles.answers}>
-				{props.answers.map((answer, index) => {
+				{props.answers.map((answer) => {
 					return (
 						<li key={answer}>
-							<input onClick={() => props.onClick(props.question, answer)} type="radio" aria-current={props.selected_answer === answer} name={props.question} id={answer} value={answer} disabled={props.checked} />
+							<input onClick={() => props.onClick(props.question, answer)} type="radio" aria-current={props.selected_answer === answer} name={props.question} id={answer} value={answer} defaultChecked={props.selected_answer === answer} disabled={props.checked} />
 							<label className={styles.answer} htmlFor={answer}>{answer}</label>
 						</li>
 					)
