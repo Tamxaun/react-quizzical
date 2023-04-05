ecacc32f7e3f7e2932a078f63be8f27ba019474b
Dmitry <deus.main@gmail.com>
2023-04-05 15:52:44 +0300

    🛠️refactor(Question) replace onClick with handleChangeAnswer method in radio buttons

--- src/components/Questions/Question/Question.tsx
+++ src/components/Questions/Question/Question.tsx
@@ -4,6 +4,12 @@ import styles from './Question.module.css';
 
 export const Question: FC<QuestionProps> = (props) => {
 
+	function handleChangeAnswer(question: string, answer: string) {
+		if (props.selected_answer) return;
+
+		props.onClick(question, answer);
+	}
+
 	return (
 		<article className={styles.question}>
 			<h3 className={styles.questionTitle}>{props.question}</h3>
@@ -11,7 +17,7 @@ export const Question: FC<QuestionProps> = (props) => {
 				{props.answers.map((answer) => {
 					return (
 						<li key={answer}>
-							<input onClick={() => props.onClick(props.question, answer)} type="radio" aria-current={props.selected_answer === answer} name={props.question} id={answer} value={answer} defaultChecked={props.selected_answer === answer} disabled={props.checked} />
+							<input onChange={() => handleChangeAnswer(props.question, answer)} type="radio" aria-current={props.selected_answer === answer} name={props.question} id={answer} value={answer} checked={props.selected_answer === answer} disabled={props.checked} />
 							<label className={styles.answer} htmlFor={answer}>{answer}</label>
 						</li>
 					)
