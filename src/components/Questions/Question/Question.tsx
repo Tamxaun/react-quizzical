655e8ed0ceddd84ceadb30e40f97b7194777e070
Dmitry <deus.main@gmail.com>
2023-04-06 16:44:18 +0300

    🛠️refactor(Question) update answer elements rendering

--- src/components/Questions/Question/Question.tsx
+++ src/components/Questions/Question/Question.tsx
@@ -5,24 +5,36 @@ import styles from './Question.module.css';
 export const Question: FC<QuestionProps> = (props) => {
 
 	function handleChangeAnswer(question: string, answer: string) {
-		if (props.selected_answer) return;
+		if (props.checked) return;
 
 		props.onClick(question, answer);
 	}
 
+	const answersElement = props.answers.map((answer) => {
+		let status = null;
+
+		if (props.checked) {
+			if (props.correct_answer === answer) {
+				status = styles.isCorrect;
+			} else {
+				status = styles.isWrong
+			}
+		} else status = null;
+
+		return (
+			<li key={answer}>
+				<input onChange={() => handleChangeAnswer(props.question, answer)} type="radio" aria-current={props.selected_answer === answer} name={props.question} id={answer} value={answer} checked={props.selected_answer === answer} disabled={props.checked} />
+				<label className={`${styles.answer} ${status ? status : null}`} htmlFor={answer}><span>{answer}</span></label>
+			</li>
+		)
+	})
+
 	return (
-		<article className={styles.question}>
-			<h3 className={styles.questionTitle}>{props.question}</h3>
-			<ul className={styles.answers}>
-				{props.answers.map((answer) => {
-					return (
-						<li key={answer}>
-							<input onChange={() => handleChangeAnswer(props.question, answer)} type="radio" aria-current={props.selected_answer === answer} name={props.question} id={answer} value={answer} checked={props.selected_answer === answer} disabled={props.checked} />
-							<label className={styles.answer} htmlFor={answer}>{answer}</label>
-						</li>
-					)
-				})}
-			</ul>
+		<article className={`${styles.question} ${props.selected_answer && styles.isSelected}`}>
+			<section>
+				<h3 className={styles.questionTitle}>{props.question}</h3>
+				<ul className={styles.answers}>{answersElement}</ul>
+			</section>
 		</article>
 	)
 };
