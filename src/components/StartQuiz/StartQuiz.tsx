fd9c4cfcd1d886bb5d0848d627e6329f777acc88
Dmitry <deus.main@gmail.com>
2023-04-16 13:50:30 +0300

    🚀feat(StartQuiz) add h1, description and start quiz button styling

--- src/components/StartQuiz/StartQuiz.tsx
+++ src/components/StartQuiz/StartQuiz.tsx
@@ -1,6 +1,17 @@
 import { FC } from 'react';
 import { StartQuizProps } from '.';
 
+const styles = {
+	button: {
+		minWidth: '192px'
+	}
+}
+
 export const StartQuiz: FC<StartQuizProps> = (props) => {
-	return <div {...props}></div>;
+
+	return <div {...props} className="container startQuiz">
+		<h1>Quizzical</h1>
+		<p>Some description if needed</p>
+		<button onClick={props.onClick} type='button' style={styles.button}>Start quiz</button>
+	</div>;
 };
