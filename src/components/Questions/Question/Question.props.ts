79d6de6e67f3ae15ee999d7879276e49466fe4d1
Dmitry <deus.main@gmail.com>
2023-04-16 16:06:40 +0300

    🛠️refactor(Question) rename 'runChecked' to 'hasRunChecked' in QuestionProps

--- src/components/Questions/Question/Question.props.ts
+++ src/components/Questions/Question/Question.props.ts
@@ -2,5 +2,5 @@ import { ForamtedQuestionType } from "../../../types";
 
 export interface QuestionProps extends ForamtedQuestionType {
 	onClick: (question: string, answer: string) => void;
-	runChecked: boolean
+	hasRunChecked: boolean
 };
